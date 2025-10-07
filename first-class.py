1aa2a0d1496bf0f32e4b7beb6a41d13dfedcbf0d
idodidodi <idodidodi@gmail.com>
2025-10-07 14:31:40 +0300

    Finish exercise

--- first-class.py
+++ first-class.py
@@ -14,4 +14,26 @@ def run():
   print(multiply(first, second))
   print(divide(first, second))
 
-run()
+# run()
+
+def run_b():
+  a = 8
+  a = 17
+  a = 9
+  b = 6
+  c = a + b
+  b = c + a
+  b = 8
+  print(a)
+  print(b)
+  print(c)
+
+# run_b()
+
+
+def run_c():
+  a = 8
+  b = "17"
+  print(a + int(b))
+
+run_c()
